cda7f39407d48a02c34f1f24ed3584858e56f548
itmadityajoshi <adityajoshi316@gmail.com>
2024-09-10 13:48:28 +0545

    user is creation and authentication done

--- Blog_site/blog/urls.py
+++ Blog_site/blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', blogs, name="blogs"),
     path('login/', login_view, name="login"),
     path('register/', register_view, name="register"),
+    path('logout/', logout_view, name="logout"),
 ]
--- Blog_site/blog/views.py
+++ Blog_site/blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Post
+
+
+
 # Create your views here.
-
-
 def blogs(request):
     post = Post.objects.all()
     context = {
@@ -13,8 +16,43 @@ def blogs(request):
 
 
 def login_view(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, username=email, password=password)
+        print(user)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            return redirect('login')
+    
     return render(request, 'auth/login.html')
 
 
 def register_view(request):
+    if request.method == "POST":
+        fname = request.POST['first_name']
+        lname = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['password_confirmation']
+
+        if password != cpassword:
+            return redirect('register')
+        else:
+            user = User.objects.create(username=email, email=email, password=cpassword)
+            user.last_name = lname
+            user.first_name = fname
+            user.save()
+            # messages.success(request, "Your Account Created Successfully.")
+            return redirect('login')
+        
     return render(request, 'auth/register.html')
+
+
+
+def logout_view(request):
+    
+    return redirect('login')
